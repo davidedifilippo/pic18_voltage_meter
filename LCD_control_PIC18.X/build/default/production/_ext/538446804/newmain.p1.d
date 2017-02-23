659555f986aaa221cda76311464436035bb1be74
davidedifilippo <davidedifilippo@gmail.com>
2017-02-23 12:20:48 +0100

    measure complete
    Project work perfectly. Channel 0 voltage measure and print on LCD

---
+++ LCD_control_PIC18.X/build/default/production/_ext/538446804/newmain.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/_ext/538446804/newmain.d  \
+ build/default/production/_ext/538446804/newmain.p1:  \
+ C:/Users/Davide/Desktop/Progetti_proteus_MPlabX/LCD_control_PIC18.X/newmain.c 
